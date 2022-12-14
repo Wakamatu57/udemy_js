5d6dc855af2095e52a72f16f1d5f707d1976e746
Wakamatu57 <raamentaishimasshu@gmail.com>
2022-12-14 22:54:21 +0900

    indexファイルにscriptタグ記入 script.js作成

---
+++ script.js
@@ -0,0 +1,43 @@
+/*
+const age = 17;
+if (age >= 20) {
+    console.log('私は20歳以上です');
+} else if (age >= 10) {
+    console.log('私は20歳未満ですが、10歳以上です');
+} else {
+    console.log('私は10歳未満です');
+};
+
+
+const n = 4;
+switch (n) {
+    case 1:
+        console.log('大吉です');
+        break;
+    case 2:
+        console.log('吉です');
+        break;
+    case 3:
+        console.log('小吉です');
+        break;
+    default:
+        console.log('凶です');
+        break;
+}
+*/
+/*
+let number = 1;
+while (number <= 100) {
+    
+    console.log(number);
+    number += 1;
+}
+*/
+for (let number = 1; number <= 100; number ++) {
+    if (number % 3 === 0) {
+        console.log('3の倍数です');
+    } else {
+        console.log(number);
+    }
+    
+}
